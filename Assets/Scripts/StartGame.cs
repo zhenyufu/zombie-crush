20b1402111f114db13c98c4e421eba6b1c93bc3b
Zhenyu Fu <zhenyufu@usc.edu>
2015-02-22 13:54:43 -0800

    set up plane

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	private int width = 300;
+	private int height = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LoadLevel() {
+		Application.LoadLevel("GameScene");
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect ((Camera.main.pixelWidth-width)/2, (Camera.main.pixelHeight-width)/2, width, height), "START GAME")) {
+			LoadLevel();	
+		}
+		
+	}
+}
