e1afb5dd3e910526b005c8dc2baffb64a4b855e6
Zhenyu Fu <zhenyufu@usc.edu>
2015-02-22 16:05:39 -0800

    green squares as moving zombies, all values hardcoded

---
+++ Assets/Scripts/ZombieFactory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieFactory : MonoBehaviour {
+
+	public GameObject zombiePrefab;
+	public float time = 1f;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("makeZombie", 2f, time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void makeZombie(){
+		GameObject zombie = Instantiate( zombiePrefab ) as GameObject;
+
+		zombie.transform.position = new Vector3(Random.Range(-5.0f, 5.0f),1,-5);
+
+	}
+
+}
---
+++ Assets/Scripts/ZombieMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieMove : MonoBehaviour {
+
+	public static float bottom = 5f;
+	public float speed = 2f;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (0.0f, 4.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 pos = transform.position;
+		pos.z += speed * Time.deltaTime;
+		transform.position = pos;
+
+		if ( transform.position.z > bottom ) {
+			Destroy( this.gameObject ); 			
+		}
+
+
+
+
+	}
+}
