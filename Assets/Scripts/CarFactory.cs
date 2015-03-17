7b47f277e880b6e576d599183c9f9aceec0d861b
Yuan Fu <fuyuanx@gmail.com>
2015-03-16 17:42:51 -0700

    change to 8 lanes

--- Assets/Scripts/CarFactory.cs
+++ Assets/Scripts/CarFactory.cs
@@ -21,8 +21,10 @@ public class CarFactory : MonoBehaviour {
 			{
 				Vector3 newPosition = hit.point;
 				GameObject car=Instantiate( carPrefab ) as GameObject;
-				//car.transform.position=new Vector3(10f, 0f, newPosition.z);
-				if(newPosition.z<=20f && newPosition.z>10f){
+
+
+				car.transform.position=new Vector3(10f, 0f, z);
+				/*if(newPosition.z<=20f && newPosition.z>10f){
 				car.transform.position=new Vector3(10f, 0f, 15f);
 				}
 				else if(newPosition.z<=10f && newPosition.z>0f){
@@ -33,6 +35,30 @@ public class CarFactory : MonoBehaviour {
 				}
 				if(newPosition.z<=-10f && newPosition.z>=-20f){
 					car.transform.position=new Vector3(10f, 0f, -15f);
+				}*/
+				if(newPosition.z<=20f && newPosition.z>15f){
+					car.transform.position=new Vector3(10f, 0f, 17.5f);
+				}
+				else if(newPosition.z<=15f && newPosition.z>10f){
+					car.transform.position=new Vector3(10f, 0f, 12.5f);
+				}
+				if(newPosition.z<=10f && newPosition.z>5f){
+					car.transform.position=new Vector3(10f, 0f, 7.5f);
+				}
+				if(newPosition.z<=5f && newPosition.z>0f){
+					car.transform.position=new Vector3(10f, 0f, 2.5f);
+				}
+				if(newPosition.z<=0f && newPosition.z>-5f){
+					car.transform.position=new Vector3(10f, 0f, -2.5f);
+				}
+				else if(newPosition.z<=-5f && newPosition.z>-10f){
+					car.transform.position=new Vector3(10f, 0f, -7.5f);
+				}
+				if(newPosition.z<=-10f && newPosition.z>-15f){
+					car.transform.position=new Vector3(10f, 0f, -12.5f);
+				}
+				if(newPosition.z<=-15f && newPosition.z>=-20f){
+					car.transform.position=new Vector3(10f, 0f, -17.5f);
 				}
 			}
 		}
