82473185dde70c4a093cd2f699360d942692b4ad
Hao Cui <hcui@usc.edu>
2015-03-23 16:04:23 -0700

    Zombie stop after collsion

--- Assets/Scripts/CarMove.cs
+++ Assets/Scripts/CarMove.cs
@@ -1,45 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CarMove : MonoBehaviour {
+public class CarMove : MonoBehaviour
+{
 
 	public static GameObject plane;// = GameObject.Find("Plane");
 	public float speed = 4f;
 	public AudioClip crash;
+	bool collided;
+
 	// Use this for initialization
-	void Start () {
-		plane = GameObject.Find("Plane");
+	void Start ()
+	{
+		plane = GameObject.Find ("Plane");
+		collided = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 		Vector3 pos = transform.position;
 		pos.x -= speed * Time.deltaTime;
 		transform.position = pos;
 
 
-		if ( transform.position.x < -10f ) {
-			Destroy( this.gameObject ); 			
+		if (transform.position.x < -10f) {
+			Destroy (this.gameObject); 			
 		}
-		
+
+		if (collided = true) {
+			if (Time.fixedTime % .5 < .2) {
+				renderer.enabled = false;
+			} else {
+				renderer.enabled = true;
+			}
+		}
 	}
 
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Zombie") {
+			AudioSource.PlayClipAtPoint (crash, collider.transform.position, 1f);
 
-	void OnTriggerEnter(Collider other){
-		if(other.gameObject.tag == "Zombie"){
-			AudioSource.PlayClipAtPoint(crash, collider.transform.position, 1f);
-			other.animation.CrossFade("Dead",0.2f);
-			Destroy(other.gameObject,1.3f);   
-			Destroy( this.gameObject );
-			ScoreBoard.DestoyOneZombie();
+			ZombieMove zmScript = other.GetComponent<ZombieMove> ();
+			zmScript.afterCollide ();
+			other.animation.CrossFade ("Dead", 0.2f);
+
+			collided = true;
+			//InvokeRepeating ("CarBlink", 0f, 1f);
+			Destroy (other.gameObject, 1.3f);   
+			Destroy(this.gameObject,1.3f);
+			//yield return new WaitForSeconds (1f);
+
+			ScoreBoard.DestoyOneZombie ();
 
 			//print(ScoreBoard.CurrentScore());
 		}
 	}
 
-
-
-
-
+	IEnumerator CarBlink ()
+	{
+		renderer.enabled = false;
+		yield return new WaitForSeconds (0.5f);
+		renderer.enabled = true;
+	}
 }
--- Assets/Scripts/ZombieMove.cs
+++ Assets/Scripts/ZombieMove.cs
@@ -1,29 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ZombieMove : MonoBehaviour {
+public class ZombieMove : MonoBehaviour
+{
 
 	public static GameObject plane;// = GameObject.Find("Plane");
 	public float speed = 2f;
+	bool collided;
+
 	// Use this for initialization
-	void Start () {
-		plane = GameObject.Find("Plane");
+	void Start ()
+	{
+		plane = GameObject.Find ("Plane");
+		collided = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-		Vector3 pos = transform.position;
-		pos.z -= speed * Time.deltaTime;
-		transform.position = pos;
+	void Update ()
+	{
+		if (!collided) {
+			Vector3 pos = transform.position;
+			pos.z -= speed * Time.deltaTime;
+			transform.position = pos;
 
-		if ( transform.position.z < -ZombieFactory.start ) {
-			Destroy( this.gameObject ); 
-			ScoreBoard.fire();
+			if (transform.position.z < -ZombieFactory.start) {
+				Destroy (this.gameObject); 
+				ScoreBoard.fire ();
+			}
 		}
+	}
 
-
-
-
+	public void afterCollide() {
+		collided = true;
 	}
 }
